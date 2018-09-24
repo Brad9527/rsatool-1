8532e2cb2e9abcd6c120ec2b4f7d6f82fa5b7804
xrust <xrust@github.com>
2018-09-24 11:54:31 +0200

    Improve factorization

--- rsatool/Factorization.py
+++ rsatool/Factorization.py
@@ -1,4 +1,7 @@
 from libnum import *
+import requests
+from lxml.html import fromstring
+
 
 class Factorization():
     def __init__(self, n, e):
@@ -9,12 +12,18 @@ class Factorization():
         self.q = None
 
     def crack(self):
-        p, q = factorize(self.n).keys()
-        phi = (p - 1) * (q - 1)
+        try:
+            response = requests.get('https://factordb.com/index.php?query={}'.format(self.n))
+            html = fromstring(response.text)
+            factors = html.findall('.//font')
+            self.p = long(factors[1].text)
+            self.q = long(factors[2].text)
+        except:
+            self.p, self.q = factorize(self.n).keys()
+
+        phi = (self.p - 1) * (self.q - 1)
         if has_invmod(self.e, phi):
             self.d = invmod(self.e, phi)
-            self.p = p
-            self.q = q
             return True
         else:
             return False
