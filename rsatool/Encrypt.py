2afb97bc64140ee680ac3d65fbb57ee441601fd8
xrust <xrust@github.com>
2018-10-01 20:52:30 +0200

    Rewrite encrypt

--- rsatool/Encrypt.py
+++ rsatool/Encrypt.py
@@ -1,17 +1,14 @@
 from Crypto.PublicKey import RSA
+import binascii
 
 class Encrypt():
-    def __init__(self, e, plaintext, keylength=1024):
-        # Generate a public/private key pair 
-        #   1024 bits key length (128 bytes)
-        #   2048 bits key length (256 bytes)
-        #   4096 bits key length (512 bytes)
-        keypair = RSA.generate(keylength, e=e)
-        c = int(keypair.encrypt(plaintext, keypair.publickey)[0].encode('hex'), 16)
+    def __init__(self, n, e, plaintext):
+        key = RSA.construct((n, e))
+        
+        c = key.encrypt(plaintext, None)[0]
+        c = '0x' + binascii.hexlify(c)
+        c = long(c, 16)
 
-        print('p = {}'.format(keypair.p))
-        print('q = {}'.format(keypair.q))
-        print('n = {}'.format(keypair.n))
-        print('e = {}'.format(keypair.e))
-        print('d = {}'.format(keypair.d))
+        print('n = {}'.format(n))
+        print('e = {}'.format(e))
         print('c = {}'.format(c))
